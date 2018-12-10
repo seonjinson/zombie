1e9a40a76c21e1b81b0c1b8a34a84b2deff0d87a
seonjinson <akqmfspt07@naver.com>
2018-12-10 14:13:31 +0900

    first init

---
+++ contracts/zombiefeeding.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.19;
+
+import "./zombiefactory.sol";
+
+contract ZombieFeeding is ZombieFactory {
+
+  function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+    require(msg.sender == zombieToOwner[_zombieId]);
+    Zombie storage myZombie = zombies[_zombieId];
+    // 여기서 시작
+  }
+
+}
