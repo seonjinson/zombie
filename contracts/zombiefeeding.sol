1e20baf1850768829c18d9fd3eeabd50ae54b4dc
seonjinson <akqmfspt07@naver.com>
2018-12-10 14:53:32 +0900

    crypto kitty interface 적용

--- contracts/zombiefeeding.sol
+++ contracts/zombiefeeding.sol
@@ -1,6 +1,20 @@
 pragma solidity ^0.4.19;
 
 import "./zombiefactory.sol";
+contract KittyInterface {
+  function getKitty(uint256 _id) external view returns (
+    bool isGestating,
+    bool isReady,
+    uint256 cooldownIndex,
+    uint256 nextActionAt,
+    uint256 siringWithId,
+    uint256 birthTime,
+    uint256 matronId,
+    uint256 sireId,
+    uint256 generation,
+    uint256 genes
+  );
+}
 
 contract ZombieFeeding is ZombieFactory {
 
